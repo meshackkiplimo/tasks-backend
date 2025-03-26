fa312cc01e057c1419376850088d4b9ee4bf0983
kimaiyo <meshackkimaiyo5@gmail.com>
2025-03-26 20:49:13 +0300

    fix:added cors

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,27 @@ import { connectDB } from "./config/db";
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+    'https://fanya-theta.vercel.app',
+    'http://localhost:5000',
+  ];
+
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    })
+  );
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Server is running!");
