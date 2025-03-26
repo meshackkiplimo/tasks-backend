5290647fe6e8b9ac241c28f00230f8ce105864b5
kimaiyo <meshackkimaiyo5@gmail.com>
2025-03-26 15:35:51 +0300

    added a route handeler

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+  });
+  
 
 // Routes
 app.use("/api/tasks", taskRoutes);
